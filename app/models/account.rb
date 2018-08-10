3f3778d83daef4148c915c96cc82ccc1621abe1f
Michael <michkna3@gmail.com>
2018-08-09 23:58:54 -0400

    round can get all account ids

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,12 @@
 class Account < ApplicationRecord
   has_many :comments, dependent: :destroy
-  #belongs_to :round
+  #has_many :matched_accounts, through: :round
+  belongs_to :round
   belongs_to :user
 
   validates_presence_of :website, :username
+  # 
+  # def matched_accounts
+  #   account = self.round.account_ids
+  # end
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,7 @@
 class Round < ApplicationRecord
   has_many :accounts
+
+  def account_ids
+    self.accounts.pluck(:id)
+  end
 end
