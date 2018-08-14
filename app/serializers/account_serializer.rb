e1d1a7f0812b7bc722ec2a07184ce424b8f205f7
Michael <michkna3@gmail.com>
2018-08-13 22:40:11 -0400

    add matched accounts

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,10 +1,15 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :website, :username
+  attributes :id, :website, :username, :matched_accounts
   has_many :comments
   def comments
     self.object.comments.map do |comment|
       {text: comment.text,
        created_at: comment.created_at}
+    end
+  end
+  def matched_accounts
+    self.object.matched_accounts(2).map do |account|
+      {username: account.username}
     end
   end
   #def round
