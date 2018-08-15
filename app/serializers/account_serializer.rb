aeee9bc07c9b6a1d9252b9b84b8104922a8117b5
Michael <michkna3@gmail.com>
2018-08-14 23:58:20 -0400

    serializer remove own comments and include matched comments in response

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,15 +1,16 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :id, :website, :username, :comments, :matched_accounts
+  attributes :id, :website, :username, :matched_info
 
-  def comments
-    self.object.comments.map do |comment|
-      {text: comment.text,
-       created_at: comment.created_at}
-    end
-  end
-  def matched_accounts
+  # def comments
+  #   self.object.comments.map do |comment|
+  #     {text: comment.text,
+  #      created_at: comment.created_at}
+  #   end
+  # end
+  def matched_info
     self.object.matched_accounts(1).map do |account|
-      {username: account.username}
+      {username: account.username,
+       comment: account.comments.first.text}
     end
   end
   #def round
